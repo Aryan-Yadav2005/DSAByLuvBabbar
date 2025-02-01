0c0e8b993e986450f51dda3345a444ba331bb104
Aryan-Yadav2005 <aaryanyadav.088@gmail.com>
2025-02-01 20:48:12 +0530

    Revert "Changes Made"
    This reverts commit d521c904d5fb79498e1b6e178249fada061ed3a5.

--- L-3/NumberisPositiveOrNegative.cpp
+++ L-3/NumberisPositiveOrNegative.cpp
@@ -1,21 +1,5 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-    int a;
-    cout << "Enter a value for a :";
-    cin >> a;
-
-    if (a > 0)
-    {
-        cout << "a is positive " << endl;
-    }
-    else if (a < 0)
-    {
-        cout << "a is negative" << endl;
-    }
-    else
-    {
-        cout << "a is equal to 0" << endl;
-    }
+int main (){
+    cout<<"Hello"<< endl;
 }
