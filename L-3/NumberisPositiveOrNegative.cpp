d521c904d5fb79498e1b6e178249fada061ed3a5
Aryan-Yadav2005 <aaryanyadav.088@gmail.com>
2025-02-01 20:36:33 +0530

    Changes Made

--- L-3/NumberisPositiveOrNegative.cpp
+++ L-3/NumberisPositiveOrNegative.cpp
@@ -1,5 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-    cout<<"Hello"<< endl;
+int main()
+{
+    int a;
+    cout << "Enter a value for a :";
+    cin >> a;
+
+    if (a > 0)
+    {
+        cout << "a is positive " << endl;
+    }
+    else if (a < 0)
+    {
+        cout << "a is negative" << endl;
+    }
+    else
+    {
+        cout << "a is equal to 0" << endl;
+    }
 }
